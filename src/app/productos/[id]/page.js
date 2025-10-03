6236dc96ac311d420b21fe097a957a16ea685160
Carlos Castro <castrovca@yandex.com>
2025-10-03 11:59:21 -0600

    se agregan productos y la visualización de los mismos

---
+++ src/app/productos/[id]/page.js
@@ -0,0 +1,17 @@
+import { productos } from "@/app/data/productos";
+
+export default function PaginaDetalleProducto({params}) {
+    const producto = productos.find(p => p.id === Number(params.id));
+
+    if(!producto) {
+        return <div>Producto no encontrado</div>;
+    }
+
+    return (
+        <div>
+            <h1>{producto.nombre}</h1>
+            <p>Precio: ${producto.precio}</p>
+            <p>{producto.descripcion}</p>
+        </div>
+    )
+}
