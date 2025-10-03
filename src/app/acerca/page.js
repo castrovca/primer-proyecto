595edccea156becc0694fd618816951ec3e23408
Carlos Castro <castrovca@yandex.com>
2025-10-03 11:30:43 -0600

    quito el link para volver atras ya que esa funcion la cumple la barra de navegacion

--- src/app/acerca/page.js
+++ src/app/acerca/page.js
@@ -6,7 +6,7 @@ export default function PaginaAcerca() {
             <h1>Sobre nosotros</h1>
 
             <p>Somos una wea de prueba. Nadie sabe muy bien por qué existimos, pero aquí estamos. Quizá por error, quizá por destino. Lo único seguro es que hacemos lo que hacemos, porque sí.</p>
-            <Link href="/">Volver atrás</Link>
+            
         </div>
 
         
